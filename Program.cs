1f4a790731a561cd00a7a1ea7a370cca89f2b9d2
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-11-26 23:00:45 -0300

    Solução do exercício Entrada Valida.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("---Entrada Valida---");
+
+int numero = -1;
+
+while (numero < 0 || numero > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+ 
+Console.WriteLine();
+ 
+if (numero == 0)
+{
+    Console.WriteLine("Operação cancelada.\n");
+}
+ 
+else {
+    Console.WriteLine($"Número selecionado = {numero}.");
+}
+ 
+Console.WriteLine("Volte sempre!\n");
